9e88152ce86ec1ae89af810d7514cb9b18da794a
jerryjohns <jerry.johns@student.moringaschool.com>
2022-06-10 00:58:44 +0300

    commit tree 9e2d616f496a10c531fc28705c414b0b56a5492a

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-// Write your solution here!
+
 const cats = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendCat (name) {
   cats.push(name);
 }
+
 function destructivelyPrependCat (name) {
-    cats.unshift(name);
-  }
-  
-  function destructivelyRemoveLastCat () {
-    cats.pop();
-  }
-  
-  function destructivelyRemoveFirstCat () {
-    cats.shift();
-  }
-  
-  function appendCat (name) {
-    return [...cats, name];
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat () {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat () {
+  cats.shift();
+}
+
+function appendCat (name) {
+  return [...cats, name];
 }
 
 function prependCat (name) {
